3d67d39d6fc25257c5a3fa10d702bbfe2f42e2b1
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-04-14 12:50:43 -0500

    Initial Commit

---
+++ Slap_Chat_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slap_Chat_iOS
+//
+//  Created by Traton Gossink on 4/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
